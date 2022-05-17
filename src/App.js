c2b118d68114dd19891119124aea7dc6e13be3d9
David <Davidfriedchicken@gmail.com>
2022-05-17 16:53:55 -0700

    Established an API, and got a flex box containing images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
+
+  let [example, setExample] = useState([])
+  let [example2, setExample2] = useState([])
+  let [example3, setExample3] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.tvmaze.com/shows')
+      .then(response => response.json())
+      .then(data => setExample(data))
+      .then(()=> fetch('https://api.tvmaze.com/shows?page=1'))
+      .then(response => response.json())
+      .then(data2 => setExample2(data2))
+  }, [])
+
+  let posterNum = []
+  if(example[0]) {
+    for(let i = 0; i < 10; i++) {
+        posterNum.splice(0, 0, example[i].image.medium)
+      }
+  }
+  for(let i = 0; posterNum.length > 6; i++) {
+    posterNum.pop()
+  }
+
+  console.log(posterNum)
+
+  let posters = posterNum.map((item) => {
+    return (
+      <div className='Movie-container'>
+        <img className='Movie' src={item} />
+      </div>
+    )
+  })
+
+  /*for(let i = 0; i < 250; i++) {
+    console.log(example[i])
+  }
+
+  for(let i = 0; i < 250; i++) {
+    console.log(example2[i])
+  } */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Posters'>
+        {posters}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+/*{array.map(function(item) {
+  return <img src={item} />
+})}*/
---
+++ src/Components/Show.js
@@ -0,0 +1,10 @@
+const show = (props) => {
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.genre}</h2>
+        </div>
+    )
+}
+
+export default show
