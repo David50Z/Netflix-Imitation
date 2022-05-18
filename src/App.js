a6a401b4217f84297c074b01505b328a2e7394f9
David <Davidfriedchicken@gmail.com>
2022-05-17 20:13:36 -0700

    Made the posters random, and sorted based on genre

--- src/App.js
+++ src/App.js
@@ -13,24 +13,30 @@ function App() {
     fetch('https://api.tvmaze.com/shows')
       .then(response => response.json())
       .then(data => setExample(data))
-      .then(()=> fetch('https://api.tvmaze.com/shows?page=1'))
+      /*.then(()=> fetch('https://api.tvmaze.com/shows?page=1'))
       .then(response => response.json())
-      .then(data2 => setExample2(data2))
+      .then(data2 => setExample2(data2))*/
   }, [])
 
-  let posterNum = []
+  /*                   JHON WICK STUFF!!!                  */
+
+
+
+
+
+  let actionNum = []
   if(example[0]) {
-    for(let i = 0; i < 10; i++) {
-        posterNum.splice(0, 0, example[i].image.medium)
+    for(let i = 0; i < example.length; i++) {
+      let random = Math.floor(Math.random() * 4)
+        if(example[i].genres.includes('Action') && random === 3){
+          actionNum.splice(0, 0, example[i].image.original)
       }
+    }
   }
-  for(let i = 0; posterNum.length > 6; i++) {
-    posterNum.pop()
+  for(let i = 0; actionNum.length > 6; i++) {
+    actionNum.pop()
   }
-
-  console.log(posterNum)
-
-  let posters = posterNum.map((item) => {
+  let actionPosters = actionNum.map((item) => {
     return (
       <div className='Movie-container'>
         <img className='Movie' src={item} />
@@ -38,18 +44,45 @@ function App() {
     )
   })
 
-  /*for(let i = 0; i < 250; i++) {
-    console.log(example[i])
-  }
 
-  for(let i = 0; i < 250; i++) {
-    console.log(example2[i])
-  } */
+
+
+
+  /*                    ADVENTURE AWAITS!!!                */
+
+
+
+
+
+  let adventureNum = []
+  if(example[0]) {
+    for(let i = 0; i < example.length; i++) {
+      let random = Math.floor(Math.random() * 4)
+        if(example[i].genres.includes('Adventure') && random === 3){
+          adventureNum.splice(0, 0, example[i].image.original)
+      }
+    }
+  }
+  for(let i = 0; adventureNum.length > 6; i++) {
+    adventureNum.pop()
+  }
+  let adventurePosters = adventureNum.map((item) => {
+    return (
+      <div className='Movie-container'>
+        <img className='Movie' src={item} />
+      </div>
+    )
+  })
+
+console.log(adventureNum)
+
+
 
   return (
     <div className="App">
       <div className='Posters'>
-        {posters}
+        {actionPosters}
+        {adventurePosters}
       </div>
     </div>
   );
